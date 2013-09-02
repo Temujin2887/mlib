dbaba32d0bfd9aee497fac7fe8c17d82ea7424a0
Nathan <temujin2887@gmail.com>
2013-09-01 20:37:06 -0700

    Remove documentation output and tweak make.bat

--- docs_source/make.bat
+++ docs_source/make.bat
@@ -5,7 +5,7 @@ REM Command file for Sphinx documentation
 if "%SPHINXBUILD%" == "" (
 	set SPHINXBUILD=sphinx-build
 )
-set BUILDDIR=../documentation
+set BUILDDIR=../../mutil-docs
 set ALLSPHINXOPTS=-d ./.doctrees %SPHINXOPTS% .
 set I18NSPHINXOPTS=%SPHINXOPTS% .
 if NOT "%PAPER%" == "" (
