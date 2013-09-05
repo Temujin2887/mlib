b9b353a0ce19990a4ba26fa8b5210384e9eb19be
Danny Wynne <wynne100@gmail.com>
2013-09-05 00:06:14 -0700

    getting the dock to behave exactly like original channel box

--- channel_box/main.py
+++ channel_box/main.py
@@ -2,9 +2,10 @@ __author__ = 'Danny Wynne'
 
 from ..core import qt
 from ..core.qt import QtGui, QtCore
+reload(qt)
 
 baseclass = qt.loadUiFile("main")
 
 class ChannelBox(baseclass):
-	def __init__(self, parent = qt.getMayaWindow()):
-		super(baseclass, self).__init__(parent)
+	def __init__(self, parent = None):
+		super(ChannelBox, self).__init__(parent)
